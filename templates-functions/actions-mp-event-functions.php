781825e66508229f73c19342a60a6cad078f3d2c
lubov9994 <luba9994@rambler.ru>
2016-05-18 17:30:12 +0300

    v1.0.7

--- templates-functions/actions-mp-event-functions.php
+++ templates-functions/actions-mp-event-functions.php
@@ -48,7 +48,7 @@ function mptt_event_template_content_time_list() { ?>
 					<p class="event-description"><?php echo $event->description; ?></p>
 				<?php } ?>
 				<?php if (!empty($event->user)) { ?>
-					<p class="event-user"><a href="<?php echo get_author_posts_url($event->user->ID); ?>"><?php echo get_avatar( $event->user->ID, apply_filters('mptt-column-user-avatar-size', 32) );
+					<p class="event-user"><a href="<?php echo get_author_posts_url($event->user->ID); ?>"><?php echo get_avatar( $event->user->ID, apply_filters('mptt-column-user-avatar-size', 32) ) . ' ';
 							echo $event->user->display_name ?></a></p>
 				<?php } ?>
 			</li>
