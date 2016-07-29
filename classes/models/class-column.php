74ea29eccbdc1887f49103174baa3ba0ba10591d
alexm <alexander.matienko@gmail.com>
2016-07-29 11:31:50 +0300

    test html

--- classes/models/class-column.php
+++ classes/models/class-column.php
@@ -135,8 +135,49 @@ class Column extends Model {
 	/**
 	 * Render meta data
 	 */
-	public static function render_column_metas(){
-		mptt_column_template_content_events_list();
+	public function render_column_metas(){
+		//mptt_column_template_content_events_list();
+		$this->appendEvents();
+	}
+	
+	public function appendEvents() {
+		?>
+
+		<?php foreach (mptt_get_column_events() as $event): ?>
+			<p class="event" id="event_columns_<?php echo $event->event_id ?>" style="overflow:auto;">
+
+				<?php if (has_post_thumbnail($event->event_id)) {
+					echo get_the_post_thumbnail( $event->event_id, apply_filters('mptt_event_thumbnail_size', 'thumbnail'), array('class' => "alignleft event-thumbnail") );
+				} else { ?>
+					<img class="alignleft event-thumbnail event-thumbnail-default" src="<?php echo \Mp_Time_Table::get_plugin_url() . 'media/css/images/column_icon.png' ?>">
+				<?php } ?>
+
+				<a href="<?php echo $event->post->timetable_disable_url == '1' ? '#' : ($event->post->timetable_custom_url != "" ? $event->post->timetable_custom_url : get_permalink($event->event_id)) ?>" class="event-link">
+					<?php echo get_the_title($event->event_id); ?>
+				</a>
+
+				<br />
+				<span class="timeslot-start"><?php echo date(get_option('time_format'), strtotime($event->event_start)); ?></span><?php echo apply_filters('mptt_timeslot_delimiter', ' - '); ?><span class="timeslot-end"><?php echo date(get_option('time_format'), strtotime($event->event_end)); ?></span>
+
+				<?php if (!empty($event->post->sub_title)) { ?>
+					<br />
+					<span class="event-subtitle"><?php echo $event->post->sub_title ?></span>
+				<?php } ?>
+
+				<?php if (!empty($event->description)) { ?>
+					<br />
+					<span class="event-description"><?php echo $event->description; ?></span>
+				<?php } ?>
+
+				<?php if (!empty($event->user)) { ?>
+					<br />
+					<span class="event-user"><a href="<?php echo get_author_posts_url($event->user->ID); ?>"><?php echo get_avatar( $event->user->ID, apply_filters('mptt-column-user-avatar-size', 32) ); ?>
+							<?php echo $event->user->display_name ?></a></span>
+				<?php } ?>
+			</p>
+		<?php endforeach; ?>
+		
+		<?php
 	}
 
 	/**
