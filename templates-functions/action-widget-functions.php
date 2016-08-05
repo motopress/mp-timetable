f0caf905ccf53ea80e48c77942c14cc2bad31b36
lubov9994 <luba9994@rambler.ru>
2016-08-05 12:07:17 +0300

    tested with IronMass Theme

--- templates-functions/action-widget-functions.php
+++ templates-functions/action-widget-functions.php
@@ -3,7 +3,7 @@ function mptt_widget_template_before_content() {
 	$wrapper_class = mptt_popular_theme_class();
 	if (\mp_timetable\classes\models\Settings::get_instance()->is_plugin_template_mode()) {
 		?>
-		<div class="widget_recent_entries  <?php echo apply_filters('mptt_widget_wrapper_class', 'upcoming-events-widget' . $wrapper_class) ?>">
+		<div class="<?php echo apply_filters('mptt_widget_wrapper_class', 'upcoming-events-widget' . $wrapper_class) ?>">
 		<ul class="mptt-widget <?php echo apply_filters('mptt_events_list_class', 'events-list') ?>">
 		<?php
 	} else {
