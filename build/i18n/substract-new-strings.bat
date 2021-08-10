7d9754ebd562b84afceb06c583b1f2b3f0c6f4ef
MotoTeam <alexander.matienko@gmail.com>
2021-08-11 01:30:10 +0300

    Feature/plugins offer (#14)
    * plugins-offer

--- build/i18n/substract-new-strings.bat
+++ build/i18n/substract-new-strings.bat
@@ -1,4 +1,4 @@
-rem https://wp-kama.ru/handbook/cli/wp/i18n
 @echo off
-wp i18n make-pot ..\.. ..\..\languages\mp-timetable-new.pot --subtract="..\..\languages\mp-timetable.pot"
+
+wp i18n make-pot ..\.. ..\..\languages\mp-timetable-new.pot --exclude="media/js/blocks/src" --subtract="..\..\languages\mp-timetable.pot"
 pause
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const config = {
 		'./media/js/mptt-functions.min.js': './media/js/mptt-functions.js',
 		'./media/js/mptt-elementor-editor.min.js': './media/js/mptt-elementor-editor.js',
 		'./media/js/mce-timeTable-buttons.min.js': './media/js/mce-timeTable-buttons.js',
+		'./media/js/mptt-option-ajax.min.js': './media/js/mptt-option-ajax.js',
 		'./media/js/events/event.min.js': './media/js/events/event.js',
 	},
 	output: {
